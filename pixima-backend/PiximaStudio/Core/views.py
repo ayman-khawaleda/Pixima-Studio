42f78bf2a0183573ffd6d95281c04bc574ba0367
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-01 18:53:23 +0300

    Setup CoreApp And Connect With Index Page

--- pixima-backend/PiximaStudio/Core/views.py
+++ pixima-backend/PiximaStudio/Core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class Index(View):
+    def get(self,request):
+        context = {}
+        return render(request=request,template_name='index.html',context=context)
