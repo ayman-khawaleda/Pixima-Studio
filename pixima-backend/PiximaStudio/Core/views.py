208a137db864828017177af5abab0838faac7dac
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-01 19:04:20 +0300

    Fix Index View

--- pixima-backend/PiximaStudio/Core/views.py
+++ pixima-backend/PiximaStudio/Core/views.py
@@ -4,6 +4,7 @@ from django.views import View
 # Create your views here.
 
 class Index(View):
+    template_name = 'index.html'
     def get(self,request):
         context = {}
-        return render(request=request,template_name='index.html',context=context)
+        return render(request=request,template_name=self.template_name,context=context)
