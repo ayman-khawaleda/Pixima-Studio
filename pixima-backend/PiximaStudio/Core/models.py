05e5a18f3f28ac1ac2f63f853141acaf4df7d03a
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-04 01:18:39 +0300

    FIX Upload Image Model

--- pixima-backend/PiximaStudio/Core/models.py
+++ pixima-backend/PiximaStudio/Core/models.py
@@ -9,7 +9,7 @@ def upload_to(ins,filename):
 class UploadImageModel(models.Model):
     class Meta:
         db_table = 'uploaded_image'
-        ordring = ('upload_time')
+        ordering = ('upload_time',)
     id = models.UUIDField(default=uuid4,primary_key=True,unique=True,db_index=True,editable=False)
     Image = models.ImageField(upload_to=upload_to)
     upload_time = models.DateTimeField(auto_now_add=True)
