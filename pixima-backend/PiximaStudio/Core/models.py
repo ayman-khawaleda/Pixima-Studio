997ca1a1756236a7aeef275b3128cce1824f03cc
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-03 14:02:54 +0300

    Update Table Name

--- pixima-backend/PiximaStudio/Core/models.py
+++ pixima-backend/PiximaStudio/Core/models.py
@@ -7,5 +7,7 @@ def upload_to(ins,filename):
     return f'{os.path.join("Images",str(uuid4())+"."+filename.split(".")[-1])}'
 
 class UploadImageModel(models.Model):
+    class Meta:
+        db_table = 'uploaded_image'
     Image = models.ImageField(upload_to=upload_to)
     upload_time = models.DateTimeField(auto_now_add=True)
