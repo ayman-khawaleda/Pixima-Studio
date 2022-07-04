9f823b1a9db00a21c23a55330c9891ffba9f6638
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-04 23:04:21 +0300

    Image Index Checking

--- pixima-backend/PiximaStudio/BasicPhotoTools/serializerHandler.py
+++ pixima-backend/PiximaStudio/BasicPhotoTools/serializerHandler.py
@@ -2,6 +2,8 @@ from abc import abstractmethod, ABCMeta
 from rest_framework.serializers import Serializer
 from . import serializer
 from Core.models import UploadImageModel
+from PiximaStudio.settings import MEDIA_ROOT
+import os
 
 
 class SerializerHandler(metaclass=ABCMeta):
@@ -24,6 +26,19 @@ class ImageSerializerHandler(SerializerHandler):
         if preview_optinos is None:
             self.preview_options = ["None", "Low", "Mid", "High"]
 
+    def __image_index_exists(self):
+        directory_id = self.serializer.data["id"]
+        index = self.serializer.data["ImageIndex"]
+        return (
+            True
+            if os.path.exists(
+                os.path.join(
+                    MEDIA_ROOT, "Images", str(directory_id), str(index) + ".jpg"
+                )
+            )
+            else False
+        )
+
     def handle(self) -> bool:
         res = self.serializer.is_valid()
         if res:
@@ -36,15 +51,18 @@ class ImageSerializerHandler(SerializerHandler):
             if self.serializer.data["ImageIndex"] < 0:
                 self.errors = {"Message": "ImageIndex Less Than 0"}
                 return False
+            if not self.__image_index_exists() and self.serializer.data["id"]:
+                self.errors = {
+                    "Message":"ImageIndex Not Found"
+                }
+                return False
             if self.serializer.data["Preview"] not in self.preview_options:
                 self.errors = {
-                    "Message": "Preview Should Be On Of This Value ['None', 'Low', 'Mid', 'High']"
+                    "Message": f"Preview Should Be On Of This Values {self.preview_options}"
                 }
                 return False
             try:
-                query_set = UploadImageModel.objects.get(
-                    id=self.serializer.data["id"]
-                )
+                query_set = UploadImageModel.objects.get(id=self.serializer.data["id"])
                 if not query_set:
                     raise UploadImageModel.DoesNotExist()
             except UploadImageModel.DoesNotExist as ex:
