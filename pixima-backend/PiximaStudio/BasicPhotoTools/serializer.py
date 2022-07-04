1cdba9ed52fff5cfed1c1c5b475cbe8b5c7593b9
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-04 12:48:35 +0300

    Add Resize Serializer

--- pixima-backend/PiximaStudio/BasicPhotoTools/serializer.py
+++ pixima-backend/PiximaStudio/BasicPhotoTools/serializer.py
@@ -20,3 +20,7 @@ class FlipImageSerializer(ImageHandlerSerializer):
 class RotateImageSerializer(ImageHandlerSerializer):
     Angle = IntegerField(default=90,required=False,allow_null=True)
     ClockWise = BooleanField(default=True,required=False,allow_null=True)
+
+class ResizeImageSerializer(ImageHandlerSerializer):
+    Width = IntegerField(default=720,required=False,allow_null=True)
+    High = IntegerField(default=-480,required=False,allow_null=True)
