7e31265d02acd374ee99e7d2ff8e7b5f30b7f95b
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-04 21:58:54 +0300

    Id Not Found In DB Excepation Added

--- pixima-backend/PiximaStudio/BasicPhotoTools/serializerHandler.py
+++ pixima-backend/PiximaStudio/BasicPhotoTools/serializerHandler.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod, ABCMeta
 from rest_framework.serializers import Serializer
 from . import serializer
+from Core.models import UploadImageModel
 
 
 class SerializerHandler(metaclass=ABCMeta):
@@ -13,12 +14,15 @@ class SerializerHandler(metaclass=ABCMeta):
         pass
 
 
-
 class ImageSerializerHandler(SerializerHandler):
-    def __init__(self, serializer: serializer.ImageHandlerSerializer,preview_optinos:list=None) -> None:
+    def __init__(
+        self,
+        serializer: serializer.ImageHandlerSerializer,
+        preview_optinos: list = None,
+    ) -> None:
         super().__init__(serializer)
         if preview_optinos is None:
-            self.preview_options = ['None','Low','Mid','High']
+            self.preview_options = ["None", "Low", "Mid", "High"]
 
     def handle(self) -> bool:
         res = self.serializer.is_valid()
@@ -27,19 +31,24 @@ class ImageSerializerHandler(SerializerHandler):
                 self.serializer.data["id"] is None
                 and self.serializer.data["Image"] is None
             ):
-                self.errors = {
-                    "Message" : "Both id and Image Can't Be Null"
-                }
+                self.errors = {"Message": "Both id and Image Can't Be Null"}
                 return False
             if self.serializer.data["ImageIndex"] < 0:
+                self.errors = {"Message": "ImageIndex Less Than 0"}
+                return False
+            if self.serializer.data["Preview"] not in self.preview_options:
                 self.errors = {
-                    "Message" : "ImageIndex Less Than 0"
+                    "Message": "Preview Should Be On Of This Value ['None', 'Low', 'Mid', 'High']"
                 }
                 return False
-            if self.serializer.data['Preview'] not in self.preview_options:
-                self.errors = {
-                    "Message" : "Preview Should Be On Of This Value ['None', 'Low', 'Mid', 'High']"
-                }
+            try:
+                query_set = UploadImageModel.objects.get(
+                    id=self.serializer.data["data"]
+                )
+                if not query_set:
+                    raise UploadImageModel.DoesNotExist()
+            except UploadImageModel.DoesNotExist as ex:
+                self.errors = {"Message": "Id Not Found"}
                 return False
         if not res:
             self.errors = self.serializer.errors
