ac0d749647be3b1704e45dce8df594b1e26dc86b
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-04 12:47:45 +0300

    Add Crop Serializer

--- pixima-backend/PiximaStudio/BasicPhotoTools/serializer.py
+++ pixima-backend/PiximaStudio/BasicPhotoTools/serializer.py
@@ -6,3 +6,9 @@ class ImageHandlerSerializer(Serializer):
     Image = ImageField(default='',required=False,allow_null=True)
     Preview = CharField(default='None',required=False)
 
+class CropImageSerializer(ImageHandlerSerializer):
+    X1 = IntegerField(default=-1,required=False,allow_null=True)
+    X2 = IntegerField(default=-1,required=False,allow_null=True)
+    Y1 = IntegerField(default=-1,required=False,allow_null=True)
+    Y2 = IntegerField(default=-1,required=False,allow_null=True)
+    Ratio = CharField(defualt='1:1',required=False,allow_null=True)
