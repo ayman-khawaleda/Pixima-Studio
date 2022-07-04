4d753723a5645c2b5285df118be312ce70ddc1ec
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-04 23:46:30 +0300

    Add Rotate Tool View And It's Url Path

--- pixima-backend/PiximaStudio/BasicPhotoTools/urls.py
+++ pixima-backend/PiximaStudio/BasicPhotoTools/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('api-crop_tool',view=views.CropToolView.as_view(),name='CropToolAPI'),
-    path('api-flip_tool',view=views.FlipToolView.as_view(),name="FlipToolAPI")
+    path('api-flip_tool',view=views.FlipToolView.as_view(),name='FlipToolAPI'),
+    path('api-rotate_tool',view=views.RotateToolView.as_view(),name='RotateToolAPI')
 ]
--- pixima-backend/PiximaStudio/BasicPhotoTools/views.py
+++ pixima-backend/PiximaStudio/BasicPhotoTools/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from django.http import JsonResponse
-from .serializer import CropImageSerializer, FlipImageSerializer
+from .serializer import CropImageSerializer, FlipImageSerializer, RotateImageSerializer
 from .serializerHandler import ImageSerializerHandler, CropImageSerializerHandler,FlipImageSerializerHandler
 
 
@@ -29,5 +29,15 @@ class FlipToolView(APIView):
         if im_handler.handle():
             return JsonResponse(
                 data={"code": HTTP_200_OK, "status": "OK", **flip_serializer.data}
+            )
+        return bad_request(im_handler.errors)
+
+class RotateToolView(APIView):
+    def post(self,request,format=None):
+        rotate_serializer = RotateImageSerializer(data=request.data)
+        im_handler = ImageSerializerHandler(rotate_serializer)
+        if im_handler.handle():
+            return JsonResponse(
+                data={"code": HTTP_200_OK, "status": "OK", **rotate_serializer.data}
             )
         return bad_request(im_handler.errors)
