14352366c398e2fc70e2dc8170d788f9d60a0295
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-05 15:42:17 +0300

    Add Flip TTool

--- pixima-backend/PiximaStudio/PiximaTools/BasicTools.py
+++ pixima-backend/PiximaStudio/PiximaTools/BasicTools.py
@@ -3,6 +3,7 @@ from PiximaTools.abstractTools import Tool
 import numpy as np
 import cv2
 
+
 class PhotoTool(Tool):
     @classmethod
     @abstractmethod
@@ -17,7 +18,6 @@ class CropTool(PhotoTool):
 
     def __call__(self, *args, **kwargs):
         return self.apply(*args, **kwargs)
-
 
     def add_cords(self, X1=-1, X2=-1, Y1=-1, Y2=-1, serializer=None):
         if serializer is not None:
@@ -37,44 +37,65 @@ class CropTool(PhotoTool):
             return False
         return True
 
-    def add_ratio(self, ratio: str=None,serializer=None):
+    def add_ratio(self, ratio: str = None, serializer=None):
         if serializer is not None:
             ratio = serializer.data["Ratio"]
         self.ratio = ratio
         return self
 
     def serializer2data(self, serializer):
-        return super()\
-            .serializer2data(serializer)\
-                .add_cords(serializer=serializer)\
-                    .add_ratio(serializer=serializer)
+        return (
+            super()
+            .serializer2data(serializer)
+            .add_cords(serializer=serializer)
+            .add_ratio(serializer=serializer)
+        )
 
     def apply(self, *args, **kwargs):
         if self.check_cords():
-            x1,x2,y1,y2 = self.cords
+            x1, x2, y1, y2 = self.cords
             croped_img = self.Image.copy()
-            croped_img = croped_img[x1:x2,y1:y2,:]
+            croped_img = croped_img[x1:x2, y1:y2, :]
             self.Image = croped_img
-            
+
         else:
             value = self.ratio
             aspect_ratio = 1
-            if value == '4:3':
-                aspect_ratio = 3/4
-            elif value == '16:9':
-                aspect_ratio = 9/16
-            elif value == '9:16':
-                aspect_ratio = 16/9
-            elif value == '5:4':
-                aspect_ratio = 4/5
-            
+            if value == "4:3":
+                aspect_ratio = 3 / 4
+            elif value == "16:9":
+                aspect_ratio = 9 / 16
+            elif value == "9:16":
+                aspect_ratio = 16 / 9
+            elif value == "5:4":
+                aspect_ratio = 4 / 5
+
             h, w, _ = self.Image.shape
-            width = int(np.min([w, h*aspect_ratio]))
-            high = int(np.min([w/aspect_ratio, h]))
-            left = int((w - width)/2)
-            top = int((h - high)/2)
+            width = int(np.min([w, h * aspect_ratio]))
+            high = int(np.min([w / aspect_ratio, h]))
+            left = int((w - width) / 2)
+            top = int((h - high) / 2)
             croped_img = self.Image.copy()
-            croped_img = croped_img[left:left+width, top:top+high]
+            croped_img = croped_img[left : left + width, top : top + high]
             self.Image = croped_img
-            
+
         return self
+
+
+class FlipTool(PhotoTool):
+    def __init__(self, direction: str = None) -> None:
+        if direction is None:
+            self.direction = "Hor"
+        self.direction = direction
+
+    def __call__(self, *args, **kwargs):
+        return self.apply(*args, **kwargs)
+
+    def add_direction(self, dir:str =None, serializer=None):
+        if serializer is not None:
+            dir = serializer.data["Direction"]
+        self.direction = dir
+        return self
+
+    def serializer2data(self, serializer):
+        return super().serializer2data(serializer).add_direction(serializer)
