cbeddd5e9b551d78cd74d142152b5f9292e6e9ba
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-05 05:19:24 +0300

    Start With Crop Tool Here

--- pixima-backend/PiximaStudio/PiximaTools/BasicTools.py
+++ pixima-backend/PiximaStudio/PiximaTools/BasicTools.py
@@ -7,3 +7,21 @@ class PhotoTool(Tool):
     def apply(self, *args, **kwargs):
         pass
 
+class CropTool(PhotoTool):
+    def __init__(self,cords:list=None,ratio:str='1:1') -> None:
+        self.cords = cords
+        self.ratio = ratio
+
+    def __call__(self, *args, **kwargs):
+        return self.apply(*args,**kwargs)
+
+    def add_cords(self,X1,X2,Y1,Y2):
+        self.cords = [X1,X2,Y1,Y2]
+        return self
+    
+    def add_ratio(self,ratio:str):
+        self.ratio = ratio
+        return self
+
+    def apply(self,*args,**kwargs):
+        return self
