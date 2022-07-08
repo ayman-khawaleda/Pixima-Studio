48b2125d850a5b18d06aa78b24bab0b53e460163
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-09 00:23:27 +0300

    Call Glitch Filter On Directory Id Request In GlitchFilterView

--- pixima-backend/PiximaStudio/PhotoFilters/views.py
+++ pixima-backend/PiximaStudio/PhotoFilters/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
-from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from django.http import JsonResponse
-from . import serializer,serializerHandler
-
+from . import serializer, serializerHandler
+from PiximaTools import Filters
 
 
 def bad_request(errors: dict):
@@ -12,37 +12,48 @@ def bad_request(errors: dict):
 
 
 class GlitchFilterView(APIView):
-
-    def post(self,request,format=None):
-        
+    def post(self, request, format=None):
+        glitch_filter = Filters.GlitchFilter()
         glicth_serializer = serializer.GlitchFilterSerializer(data=request.data)
-        filter_handler = serializerHandler.GlitchFilterSerializerHandler(glicth_serializer)
+        filter_handler = serializerHandler.GlitchFilterSerializerHandler(
+            glicth_serializer
+        )
         try:
             if filter_handler.handle():
+                image_path = (
+                    glitch_filter.serializer2data(glicth_serializer)
+                    .read_image()()
+                    .save_image()
+                )
+                imagepreview_path = glitch_filter.get_preview()
+
                 return JsonResponse(
                     data={
                         "code": HTTP_200_OK,
                         "status": "OK",
-                        **glicth_serializer.data
+                        "Image": image_path,
+                        "ImagePreview": imagepreview_path,
                     }
                 )
         except Exception as e:
             return bad_request({"Message": "Error In Glitch Filter Request"})
         return bad_request(filter_handler.errors)
 
-class CircleFilterView(APIView):
 
-    def post(self,request,format=None):
-        
+class CircleFilterView(APIView):
+    def post(self, request, format=None):
+
         circles_serializer = serializer.CirclesFilterSerializer(data=request.data)
-        filter_handler = serializerHandler.CirclesFilterSerializerHandler(circles_serializer)
+        filter_handler = serializerHandler.CirclesFilterSerializerHandler(
+            circles_serializer
+        )
         try:
             if filter_handler.handle():
                 return JsonResponse(
                     data={
                         "code": HTTP_200_OK,
                         "status": "OK",
-                        **circles_serializer.data
+                        **circles_serializer.data,
                     }
                 )
         except Exception as e:
