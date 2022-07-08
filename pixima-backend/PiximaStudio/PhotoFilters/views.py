41cd162ef90c8fba7762dd9b4f38aa50211a4c34
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-09 02:57:13 +0300

    Call Circle Filter In CircleFilterView For Directory Id Request

--- pixima-backend/PiximaStudio/PhotoFilters/views.py
+++ pixima-backend/PiximaStudio/PhotoFilters/views.py
@@ -55,25 +55,30 @@ class GlitchFilterView(APIView):
                     }
                 )
         except Exception as e:
-            print(e)
             return bad_request({"Message": "Error In Glitch Filter Request"})
         return bad_request(filter_handler.errors)
 
 
 class CircleFilterView(APIView):
     def post(self, request, format=None):
-
+        circles_filter = Filters.CirclesFilter()
         circles_serializer = serializer.CirclesFilterSerializer(data=request.data)
         filter_handler = serializerHandler.CirclesFilterSerializerHandler(
             circles_serializer
         )
         try:
             if filter_handler.handle():
+                circles_filter.serializer2data(
+                    serializer=circles_serializer
+                ).read_image().apply()
+                image_path = circles_filter.save_image()
+                imagepreview_path = circles_filter.get_preview()
                 return JsonResponse(
                     data={
                         "code": HTTP_200_OK,
                         "status": "OK",
-                        **circles_serializer.data,
+                        "Image": image_path,
+                        "ImagePreview": imagepreview_path,
                     }
                 )
         except Exception as e:
