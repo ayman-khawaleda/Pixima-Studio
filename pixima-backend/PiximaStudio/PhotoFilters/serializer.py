8c10108ec8361cd6f3722c411da2abeeb2f60d96
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-08 21:01:11 +0300

    Add Glitch Filter Serializer\Handler

--- pixima-backend/PiximaStudio/PhotoFilters/serializer.py
+++ pixima-backend/PiximaStudio/PhotoFilters/serializer.py
@@ -1,1 +1,8 @@
-from AbstractSerializer import serializer
+from rest_framework.serializers import IntegerField
+from AbstractSerializer.serializer import ImageSerializer
+
+
+class GlitchFilterSerializer(ImageSerializer):
+    Shift = IntegerField(default=20, required=False)
+    Step = IntegerField(default=15, required=False)
+    Density = IntegerField(default=5, required=False)
--- pixima-backend/PiximaStudio/PhotoFilters/serializerHandler.py
+++ pixima-backend/PiximaStudio/PhotoFilters/serializerHandler.py
@@ -1,1 +1,26 @@
 from AbstractSerializer import serializer,serializerHandler
+
+class GlitchFilterSerializerHandler(serializerHandler.ImageSerializerHandler):
+    def __init__(self, serializer: serializer.ImageSerializer, preview_optinos: list = None) -> None:
+        super().__init__(serializer, preview_optinos)
+    
+    def handle(self) -> bool:
+        res = super().handle()
+        if res:
+            shift = self.serializer.data["Shift"]
+            if shift > 50 or shift < 0:
+                self.errors = {"Message":"Shift Should Be In Range [0, 50]"}
+                return False
+            
+            step = self.serializer.data["Step"]
+            if step > 25 or step < 0:
+                self.errors = {"Message":"Step Should Be In Range [0, 25]"}
+                return False
+            
+            density = self.serializer.data["Density"]
+            if density > 50 or density < 0:
+                self.errors = {"Message":"Density Should Be In Range [0, 50]"}
+                return False
+        if not res:
+            self.errors = self.serializer.errors
+        return res
