aec8bd91ebf9991e8f2b703b0bbcdb94d7fab729
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-09 00:21:30 +0300

    def Apply Glitch Filter

--- pixima-backend/PiximaStudio/PiximaTools/Filters.py
+++ pixima-backend/PiximaStudio/PiximaTools/Filters.py
@@ -1,5 +1,8 @@
 from abc import abstractmethod
+from random import Random
 from PiximaTools.abstractTools import Tool
+import cv2
+import numpy as np
 
 
 class Filter(Tool):
@@ -14,6 +17,9 @@ class GlitchFilter(Filter):
         self.shift = shift
         self.step = step
         self.density = density
+
+    def __call__(self, *args, **kwargs):
+        return self.apply(*args, **kwargs)
 
     def serializer2data(self, serializer):
         return (
@@ -49,4 +55,39 @@ class GlitchFilter(Filter):
         return self
 
     def apply(self, *args, **kwargs):
+        img = self.Image
+        h, w, _ = img.shape
+        kernal = np.zeros((h, w), np.uint8)
+        thickness = 2
+        if h > 1000:
+            thickness = 4
+        list_range = []
+        for i in range(0, h, self.step):
+            list_range.append((i - self.step, i))
+            kernal = cv2.line(
+                kernal, (0, int(i)), (int(w), int(i)), (255, 255, 255), thickness
+            )
+        kernal = cv2.merge([kernal, kernal, kernal])
+        cyan_img = img.copy()
+        pink_img = img.copy()
+        pink_img[:, :, 1] = 0
+        cyan_img[:, :, 0] = 0
+        pink_img = np.roll(pink_img, self.shift, 1)
+        cyan_img = np.roll(cyan_img, -self.shift, 1)
+        new_img = cv2.addWeighted(img, 0.5, pink_img, 0.9, 0)
+        new_img = cv2.addWeighted(new_img, 0.5, cyan_img, 0.75, 0)
+        new_img = cv2.addWeighted(
+            new_img,
+            0.85,
+            kernal,
+            0.15,
+            0,
+        )
+        for i in Random().choices(list_range, k=self.density):
+            down = i[0]
+            upper = i[0] + (self.step * Random().randint(0, 3))
+            new_img[down:upper, :, :] = np.roll(
+                new_img[down:upper, :, :], Random().randint(-100, 100), 1
+            )
+        self.Image = new_img
         return self
