ba60e80b2265e7737f7ed874114c526b20ba3918
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-08 21:23:32 +0300

    Prepare Views and Glictch\Circle Filters urls

--- pixima-backend/PiximaStudio/PhotoFilters/urls.py
+++ pixima-backend/PiximaStudio/PhotoFilters/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path(
+        "api-glitch_filter",
+        view=views.GlitchFilterView.as_view(),
+        name="GlitchFilterAPI",
+    ),
+    path(
+        "api-circle_filter",
+        view=views.CircleFilterView.as_view(),
+        name="CircleFilterAPI",
+    ),
 ]
--- pixima-backend/PiximaStudio/PhotoFilters/views.py
+++ pixima-backend/PiximaStudio/PhotoFilters/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST
+from django.http import JsonResponse
+from . import serializer,serializerHandler
 
-# Create your views here.
+
+
+def bad_request(errors: dict):
+    return JsonResponse(
+        data={"code": HTTP_400_BAD_REQUEST, "status": "BAD REQUEST", **errors}
+    )
+
+
