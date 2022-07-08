a4336caee87237b1ce5e3d565cd1082ebead8cb7
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-08 23:14:32 +0300

    Add Glitch Filter & Circles Filter Views

--- pixima-backend/PiximaStudio/PhotoFilters/views.py
+++ pixima-backend/PiximaStudio/PhotoFilters/views.py
@@ -11,3 +11,40 @@ def bad_request(errors: dict):
     )
 
 
+class GlitchFilterView(APIView):
+
+    def post(self,request,format=None):
+        
+        glicth_serializer = serializer.GlitchFilterSerializer(data=request.data)
+        filter_handler = serializerHandler.GlitchFilterSerializerHandler(glicth_serializer)
+        try:
+            if filter_handler.handle():
+                return JsonResponse(
+                    data={
+                        "code": HTTP_200_OK,
+                        "status": "OK",
+                        **glicth_serializer.data
+                    }
+                )
+        except Exception as e:
+            return bad_request({"Message": "Error In Glitch Filter Request"})
+        return bad_request(filter_handler.errors)
+
+class CircleFilterView(APIView):
+
+    def post(self,request,format=None):
+        
+        circles_serializer = serializer.CirclesFilterSerializer(data=request.data)
+        filter_handler = serializerHandler.CirclesFilterSerializerHandler(circles_serializer)
+        try:
+            if filter_handler.handle():
+                return JsonResponse(
+                    data={
+                        "code": HTTP_200_OK,
+                        "status": "OK",
+                        **circles_serializer.data
+                    }
+                )
+        except Exception as e:
+            return bad_request({"Message": "Error In Circles Filter Request"})
+        return bad_request(filter_handler.errors)
