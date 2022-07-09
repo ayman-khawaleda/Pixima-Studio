abebb3f3b3e5fc3d923fc16504605aca0c7b2235
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-09 14:48:13 +0300

    Fix MinMax Range

--- pixima-backend/PiximaStudio/FaceTools/serializer.py
+++ pixima-backend/PiximaStudio/FaceTools/serializer.py
@@ -4,12 +4,12 @@ from AbstractSerializer.serializer import ImageSerializer
 
 class EyesColorSerializer(ImageSerializer):
     Color = ListField(
-        child=IntegerField(default=0, max_value=0, min_value=255),
+        child=IntegerField(default=0, min_value=0, max_value=255),
         min_length=1,
         max_length=2,
     )
     Saturation = ListField(
-        child=IntegerField(default=0, max_value=0, min_value=100),
+        child=IntegerField(default=0, min_value=0, max_value=100),
         min_length=1,
         max_length=2,
     )
