e23c69075641c800b869c9394e3456f55d567c75
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-09 14:37:16 +0300

    Add EyesColor SerializerHandler

---
+++ pixima-backend/PiximaStudio/FaceTools/serializerHandler.py
@@ -0,0 +1,8 @@
+from AbstractSerializer.serializerHandler import ImageSerializerHandler
+from . import serializer
+
+
+class EyesColorSerializerHandler(ImageSerializerHandler):
+    def handle(self) -> bool:
+        return super().handle()
+    
