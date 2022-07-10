670888543ecff5fcae6b5a57836840238524b2a8
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-10 15:52:52 +0300

    Add EyesColorTool Url and it's View

--- pixima-backend/PiximaStudio/FaceTools/views.py
+++ pixima-backend/PiximaStudio/FaceTools/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from PiximaStudio.AbstractView import RESTView
+from .serializer import EyesColorSerializer
+from .serializerHandler import EyesColorSerializerHandler
 
-# Create your views here.
+
+class EyesColorToolView(RESTView):
+    def post(self, request, format=None):
+        eyescolor_serializer = EyesColorSerializer(data=request.data)
+        eyescolor_serializerhandler = EyesColorSerializerHandler(eyescolor_serializer)
+        try:
+            if eyescolor_serializerhandler.handle():
+                return self.ok_request(eyescolor_serializerhandler.serializer.data)
+        except Exception as e:
+            return self.bad_request({"Message": "Error During Flip Process"})
+        return self.bad_request(eyescolor_serializerhandler.errors)
