c4e619da2cb8939ab84cfa9cf130f949cb511e9a
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-11 01:03:30 +0300

    Eyes Resize Tool View Just Ready

--- pixima-backend/PiximaStudio/FaceTools/urls.py
+++ pixima-backend/PiximaStudio/FaceTools/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("api-coloreyes_tool",view=views.EyesColorToolView.as_view(),name="ColorEyesToolAPI")
+    path("api-coloreyes_tool",view=views.EyesColorToolView.as_view(),name="ColorEyesToolAPI"),
+    path("api-resizeeyes_tool",view=views.EyesResizeToolView.as_view(),name="ResizeEyesToolAPI"),
 ]
--- pixima-backend/PiximaStudio/FaceTools/views.py
+++ pixima-backend/PiximaStudio/FaceTools/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from PiximaStudio.AbstractView import RESTView
-from .serializer import EyesColorSerializer
-from .serializerHandler import EyesColorSerializerHandler
+from .serializer import EyesColorSerializer, EyesResizeSerializer
+from .serializerHandler import EyesColorSerializerHandler, EyesResizeSerializerHandler
 from PiximaTools.FaceTools import EyesTool
 from PiximaTools.Exceptions import RequiredValue, NoFace
 
@@ -17,21 +17,27 @@ class EyesColorToolView(RESTView):
                 image_path = eyescolor_tool.save_image()
                 imagepreview_path = eyescolor_tool.get_preview()
                 mask_path = eyescolor_tool.save_mask()
-                return self.ok_request({
-                    "Image": image_path,
-                    "ImagePreview": imagepreview_path,
-                    "Mask":mask_path
-                })
+                return self.ok_request(
+                    {
+                        "Image": image_path,
+                        "ImagePreview": imagepreview_path,
+                        "Mask": mask_path,
+                    }
+                )
             if eyescolor_serializerhandler.handle():
-                eyescolor_tool.serializer2data(eyescolor_serializer).read_image().apply()
+                eyescolor_tool.serializer2data(
+                    eyescolor_serializer
+                ).read_image().apply()
                 image_path = eyescolor_tool.save_image()
                 imagepreview_path = eyescolor_tool.get_preview()
                 mask_path = eyescolor_tool.save_mask()
-                return self.ok_request({
-                    "Image": image_path,
-                    "ImagePreview": imagepreview_path,
-                    "Mask":mask_path
-                })
+                return self.ok_request(
+                    {
+                        "Image": image_path,
+                        "ImagePreview": imagepreview_path,
+                        "Mask": mask_path,
+                    }
+                )
         except RequiredValue as e:
             return self.bad_request({"Message": str(e)})
         except NoFace as e:
@@ -40,3 +46,22 @@ class EyesColorToolView(RESTView):
             print(e)
             return self.bad_request({"Message": "Error During Color Eyes Process"})
         return self.bad_request(eyescolor_serializerhandler.errors)
+
+
+class EyesResizeToolView(RESTView):
+    def post(self, request, format=None):
+        eyesresize_serializer = EyesResizeSerializer(data=request.data)
+        eyesresize_serializerhandler = EyesResizeSerializerHandler(
+            eyesresize_serializer
+        )
+        try:
+            if eyesresize_serializerhandler.handle():
+                return self.ok_request({"Message": eyesresize_serializer.data})
+        except RequiredValue as e:
+            return self.bad_request({"Message": str(e)})
+        except NoFace as e:
+            return self.bad_request({"Message": str(e)})
+        except Exception as e:
+            print(e)
+            return self.bad_request({"Message": "Error During Eyes Resize Process"})
+        return self.bad_request(eyesresize_serializerhandler.errors)
