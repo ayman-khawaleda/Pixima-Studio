1dbc1723a17d45783efc072268ea76fa16b6ed9a
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-10 19:37:00 +0300

    Add Face Mesh Model

--- pixima-backend/PiximaStudio/PiximaTools/AI_Models.py
+++ pixima-backend/PiximaStudio/PiximaTools/AI_Models.py
@@ -1,7 +1,15 @@
 import mediapipe as mp
 
 mp_face_detection = mp.solutions.face_detection
+mp_face_mesh = mp.solutions.face_mesh
 
 face_detection_model = mp_face_detection.FaceDetection(
     model_selection=1, min_detection_confidence=0.5
+)
+
+face_mesh_model = mp_face_mesh.FaceMesh(
+    static_image_mode=True,
+    max_num_faces=1,
+    refine_landmarks=True,
+    min_detection_confidence=0.5,
 )
