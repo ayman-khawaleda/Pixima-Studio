8f701a4e6e4b26470f6befbbd15661bc5d24bb4f
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-10 23:02:15 +0300

    Color Eyes Tool View Ready To Handle One Image Request

--- pixima-backend/PiximaStudio/FaceTools/views.py
+++ pixima-backend/PiximaStudio/FaceTools/views.py
@@ -12,6 +12,16 @@ class EyesColorToolView(RESTView):
         eyescolor_serializer = EyesColorSerializer(data=request.data)
         eyescolor_serializerhandler = EyesColorSerializerHandler(eyescolor_serializer)
         try:
+            if "Image" in request.data.keys() and request.data["Image"] != "":
+                eyescolor_tool.request2data(request=request)()
+                image_path = eyescolor_tool.save_image()
+                imagepreview_path = eyescolor_tool.get_preview()
+                mask_path = eyescolor_tool.save_mask()
+                return self.ok_request({
+                    "Image": image_path,
+                    "ImagePreview": imagepreview_path,
+                    "Mask":mask_path
+                })
             if eyescolor_serializerhandler.handle():
                 eyescolor_tool.serializer2data(eyescolor_serializer).read_image().apply()
                 image_path = eyescolor_tool.save_image()
@@ -27,5 +37,6 @@ class EyesColorToolView(RESTView):
         except NoFace as e:
             return self.bad_request({"Message": str(e)})
         except Exception as e:
+            print(e)
             return self.bad_request({"Message": "Error During Color Eyes Process"})
         return self.bad_request(eyescolor_serializerhandler.errors)
