9420f2a6bace8422af3222970dfea2c0f6428b6f
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-10 19:05:54 +0300

    Fix Face Detector Name

--- pixima-backend/PiximaStudio/PiximaTools/AI_Models.py
+++ pixima-backend/PiximaStudio/PiximaTools/AI_Models.py
@@ -2,6 +2,6 @@ import mediapipe as mp
 
 mp_face_detection = mp.solutions.face_detection
 
-face_detection_modle = mp_face_detection.FaceDetection(
+face_detection_model = mp_face_detection.FaceDetection(
     model_selection=1, min_detection_confidence=0.5
 )
