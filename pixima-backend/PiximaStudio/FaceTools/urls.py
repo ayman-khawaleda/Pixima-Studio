54b4ef0d20d6244045c1801e4a6bd05ad5a2e412
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-11 13:39:52 +0300

    Add NoseResizeTool Url

--- pixima-backend/PiximaStudio/FaceTools/urls.py
+++ pixima-backend/PiximaStudio/FaceTools/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path("api-coloreyes_tool",view=views.EyesColorToolView.as_view(),name="ColorEyesToolAPI"),
     path("api-resizeeyes_tool",view=views.EyesResizeToolView.as_view(),name="ResizeEyesToolAPI"),
+    path("api-resizenose_tool",view=views.NoseResizeToolView.as_view(),name="ResizeNoseToolAPI"),
 ]
