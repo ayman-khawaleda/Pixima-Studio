2478c89bcd0bd2e6a0ffaea5f16c0b1882ea67eb
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-13 23:31:49 +0300

    Color Lips Tool init And override Some Method

--- pixima-backend/PiximaStudio/PiximaTools/FaceTools/FaceTools.py
+++ pixima-backend/PiximaStudio/PiximaTools/FaceTools/FaceTools.py
@@ -569,3 +569,64 @@ class WhiteTeethTool(FaceTool):
         ] = self.teethmask
 
         return self
+
+class ColorLipsTool(FaceTool):
+    def __init__(self, faceMeshDetector=None, saturation=0, color=0) -> None:
+        if faceMeshDetector is None:
+            faceMeshDetector = face_mesh_model
+        self.faceMeshDetector = faceMeshDetector
+        self.saturation = saturation
+        self.color = color
+
+    def __call__(self, *args, **kwargs):
+        return self.apply(*args, **kwargs)
+
+    def add_saturation(self, saturation=5, serialzier=None):
+        if serialzier:
+            saturation = serialzier.data["Saturation"]
+        elif type(saturation.dict()) == dict:
+
+            class SaturationSerializer(Serializer):
+                Saturation = IntegerField(
+                    default=0, required=False, min_value=-100, max_value=100
+                )
+
+            saturation_serializer = SaturationSerializer(data=saturation)
+            if not saturation_serializer.is_valid():
+                raise RequiredValue(**saturation_serializer.errors)
+            saturation = saturation_serializer.data["Saturation"]
+        self.saturation = saturation
+        return self
+
+    def add_color(self, color=0, serialzier=None):
+        if serialzier:
+            color = serialzier.data["Color"]
+        elif type(color.dict()) == dict:
+
+            class ColorSerializer(Serializer):
+                Color = IntegerField(
+                    default=0, required=False, min_value=0, max_value=180
+                )
+
+            color_serializer = ColorSerializer(data=color)
+            if not color_serializer.is_valid():
+                raise RequiredValue(**color_serializer.errors)
+            color = color_serializer.data["Color"]
+        self.color = color
+        return self
+
+    def request2data(self, request):
+        return (
+            super()
+            .request2data(request)
+            .add_saturation(request.data)
+            .add_color(request.data)
+        )
+
+    def serializer2data(self, serializer):
+        return (
+            super()
+            .serializer2data(serializer)
+            .add_saturation(serialzier=serializer)
+            .add_color(serialzier=serializer)
+        )
