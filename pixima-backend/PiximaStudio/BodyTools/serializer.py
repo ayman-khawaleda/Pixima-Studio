fd52d2b05778a44cc26a59197a29f7d281e5ca07
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-16 10:51:06 +0300

    Color Serializer Added

--- pixima-backend/PiximaStudio/BodyTools/serializer.py
+++ pixima-backend/PiximaStudio/BodyTools/serializer.py
@@ -1,2 +1,6 @@
 from rest_framework.serializers import IntegerField
 from AbstractSerializer.serializer import ImageSerializer
+
+class ColorHairSerializer(ImageSerializer):
+    Color = IntegerField(default=0, required=False, min_value=0, max_value=180)
+    Saturation = IntegerField(default=0, required=False, min_value=0, max_value=100)
