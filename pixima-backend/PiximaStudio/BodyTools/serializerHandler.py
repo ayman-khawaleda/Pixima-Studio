ba1dbf0dad6f34d491a3291df1a8a5c6897db4a2
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-16 10:51:19 +0300

    Color SerializerHandler Added

--- pixima-backend/PiximaStudio/BodyTools/serializerHandler.py
+++ pixima-backend/PiximaStudio/BodyTools/serializerHandler.py
@@ -1,2 +1,6 @@
-from AbstractSerializer.serializer import ImageSerializer
 from AbstractSerializer.serializerHandler import ImageSerializerHandler
+from . import serializer
+
+class ColorHairSerializerHandler(ImageSerializerHandler):
+    def handle(self) -> bool:
+        return super().handle()
