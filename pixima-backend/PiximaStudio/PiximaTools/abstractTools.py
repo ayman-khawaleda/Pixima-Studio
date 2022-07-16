031e0c9a495c3da71bda12553c58f33062a24f7c
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-07-16 10:08:35 +0300

    Add BodyTool Class As Addition Abstract Layer

--- pixima-backend/PiximaStudio/PiximaTools/abstractTools.py
+++ pixima-backend/PiximaStudio/PiximaTools/abstractTools.py
@@ -1,14 +1,15 @@
 from abc import ABC, abstractmethod
-import math
 from PiximaStudio.settings import MEDIA_ROOT, MEDIA_URL
 from skimage.io import imsave, imread
 from PIL import Image
-import os
-import numpy as np
 from uuid import uuid4
 from . import Exceptions
 from Core.models import ImageOperationsModel
-
+from PiximaTools.Exceptions import ImageNotSaved
+import os
+import cv2 
+import math
+import numpy as np
 class Tool(ABC):
     @classmethod
     @abstractmethod
@@ -155,3 +156,24 @@ class Tool(ABC):
         x_px = min(math.floor(normalized_x * image_width), image_width - 1)
         y_px = min(math.floor(normalized_y * image_height), image_height - 1)
         return x_px, y_px
+
+class BodyTool(Tool,ABC):
+    @classmethod
+    @abstractmethod
+    def apply(self, *args, **kwargs):
+        pass
+
+    def save_mask(self, *args, **kwargs):
+        try:
+            sub_path = os.path.join(MEDIA_ROOT, "ImageMasks", str(self.directory_id))
+            if not os.path.exists(sub_path):
+                os.mkdir(sub_path)
+            self.lastidx = len(os.listdir(sub_path))
+            full_path = os.path.join(sub_path, f"{self.lastidx}.jpg")
+            cv2.imwrite(full_path, self.Mask)
+            mask_path = os.path.join(
+                MEDIA_URL, "ImageMasks", str(self.directory_id), f"{self.lastidx}.jpg"
+            )
+            return mask_path
+        except Exception as e:
+            raise ImageNotSaved("Error In Save Image Mask")
