038498aeeba09156841685f40f1b0e95fbe63c20
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-08-07 19:55:11 +0300

    Refactor Setting

--- pixima-backend/PiximaStudio/PiximaStudio/settings.py
+++ pixima-backend/PiximaStudio/PiximaStudio/settings.py
@@ -29,77 +29,72 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-#Rest FrameWork Configrations
-REST_FRAMWORK = {
-    'DEFULAT_RENDERER_CLASSES':(
-        'rest_framework.renderers.JSONRenderer',
-    )
-}
-MEDIA_ROOT = os.path.join(PROJECT_DIR,'pixima_media')
-MEDIA_URL = '/pixima_media/'
+# Rest FrameWork Configrations
+REST_FRAMWORK = {"DEFULAT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",)}
+MEDIA_ROOT = os.path.join(PROJECT_DIR, "pixima_media")
+MEDIA_URL = "/pixima_media/"
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'Core',
-    'BasicPhotoTools',
-    'PhotoFilters',
-    'FaceTools',
-    'BodyTools',
-    'corsheaders',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "Core",
+    "BasicPhotoTools",
+    "PhotoFilters",
+    "FaceTools",
+    "BodyTools",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
-ROOT_URLCONF = 'PiximaStudio.urls'
+ROOT_URLCONF = "PiximaStudio.urls"
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-    'access-control-allow-origin'
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "access-control-allow-origin",
 )
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_DIR,'pixima-frontend','build')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(PROJECT_DIR, "pixima-frontend", "build")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-STATICFILES_DIRS = [os.path.join(PROJECT_DIR, 'pixima-frontend', 'build', 'static')]
+STATICFILES_DIRS = [os.path.join(PROJECT_DIR, "pixima-frontend", "build", "static")]
 
-WSGI_APPLICATION = 'PiximaStudio.wsgi.application'
+WSGI_APPLICATION = "PiximaStudio.wsgi.application"
 
 
 # Database
@@ -107,13 +102,13 @@ WSGI_APPLICATION = 'PiximaStudio.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': configrations.DB_CONFIG['NAME'],
-        'USER': configrations.DB_CONFIG['USER'],
-        'PASSWORD': configrations.DB_CONFIG['PASSWORD'],
-        'HOST': configrations.DB_CONFIG['HOST'],
-        'PORT': configrations.DB_CONFIG['PORT'],
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": configrations.DB_CONFIG["NAME"],
+        "USER": configrations.DB_CONFIG["USER"],
+        "PASSWORD": configrations.DB_CONFIG["PASSWORD"],
+        "HOST": configrations.DB_CONFIG["HOST"],
+        "PORT": configrations.DB_CONFIG["PORT"],
     }
 }
 
@@ -122,16 +117,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -139,9 +134,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -151,9 +146,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
