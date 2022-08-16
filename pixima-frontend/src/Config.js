c81efb247150edb3578302a43c7433fc61bdad30
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-08-16 23:43:41 +0300

    Add Config.js and remove it from .gitignore

---
+++ pixima-frontend/src/Config.js
@@ -0,0 +1,22 @@
+export const Server = "http://localhost:8000"
+export const ServerMedia = "/pixima_media/Images"
+export const ServerImageExt = ".jpg"
+export const EndPoints = {
+    SmileToolEndPoint: "/api-smile_tool",
+    ColorLipsToolEndPoint: "/api-colorlips_tool",
+    ColorEyesToolEndPoint: "/api-coloreyes_tool",
+    ResizeEyesToolEndPoint: "/api-resizeeyes_tool",
+    ResizeNoseToolEndPoint: "/api-resizenose_tool",
+    WhiteTeethToolEndPoint: "/api-whiteteeth_tool",
+    SmoothFaceToolEndPoint: "/api-smoothface_tool",
+    ColorHairToolEndPoint: "/api-colorhair_tool",
+    ColorToolEndPoint: "/api-changecolor_tool",
+    GlitchFilterToolEndPoint: "/api-glitch_filter",
+    CirclesFilterToolEndPoint: "/api-circle_filter",
+    FlipToolEndPoint: "/api-flip_tool",
+    CropToolEndPoint: "/api-crop_tool",
+    RotateToolEndPoint: "/api-rotate_tool",
+    ResizeToolEndPoint: "/api-resize_tool",
+    ContrastToolEndPoint: "/api-contrast_tool",
+    SaturationToolEndPoint: "/api-saturation_tool",
+}
